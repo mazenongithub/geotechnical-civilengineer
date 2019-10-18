bd11a4c6cd150de9d04065e5f5259902b9da61de
mazenongithub <mazen@egeotechnical.com>
2019-10-18 12:20:32 -0700

    update to client to make it work

--- src/components/header.js
+++ src/components/header.js
@@ -72,7 +72,8 @@ class Header extends(Component) {
     if (this.props.myusermodel) {
       if (this.props.myusermodel.hasOwnProperty("clientid")) {
         //loggedin
-        let backendapi = `${process.env.REACT_APP_SERVER}/api/clientlogout`
+        let clientid = this.props.myusermodel.className;
+        let backendapi = `${process.env.REACT_APP_SERVER}/geotechnical/${clientid}/clientlogout`
         return (<p><a href={backendapi} className="nav-link">logout </a></p>)
       }
       else {
