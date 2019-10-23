feff80e33091f069550abfd87d95bec26c179fa1
mazenongithub <mazen@egeotechnical.com>
2019-10-23 12:00:06 -0700

    update to register

--- src/components/completeprofile.js
+++ src/components/completeprofile.js
@@ -320,36 +320,40 @@ class CompleteProfile extends Component {
         return gender;
     }
     render() {
-        const formaction = `${process.env.REACT_APP_SERVER}/api/${this.props.match.params.clientid}/updateclientregister`
+        const formaction = `${process.env.REACT_APP_SERVER}/${this.props.match.params.clientid}/updateclientregister`
 
-        return (<form action={formaction} method="post" onSubmit={event=>{this.handlesubmit(event)}}>
-        <div class="register-container">
-      
-        <div class="register-title">
-        Complete Your Profile
+        return (<form action={formaction} method="post" onSubmit={event => { this.handlesubmit(event) }}>
+            <div class="register-container">
+
+                <div class="register-title">
+                    Complete Your Profile
         </div>
-        <div className="register-full">{this.state.message}  </div>
-        <div className="gender-container">
-            <button type="button" className="newradio" id="btnmale" onClick={event=>{this.handlemale(event)}}>{this.returnmale()} </button> Male
-            <button type="button" className="newradio" id="btnfemale" onClick={event=>{this.handlefemale(event)}}> {this.returnfemale()}</button> Female
-            <input type="hidden" name="gender" value={this.getgender()}/>
-        </div>
-        <div className="register-half">First Name<br/><input type="text" className="regular-field" name="firstname" onChange={event=>{this.handlefirstname(event.target.value)}} value={this.getfirstname()} /></div>
-         <div className="register-half">Last Name <br/><input type="text" className="regular-field" name="lastname"  onChange={event=>{this.handlelastname(event.target.value)}} value={this.getlastname()}/></div>
-         <div className="register-full">Client ID <br/><input type="text" className="regular-field" name="clientid" onFocus={event=>{this.checkclientid(event)}} onBlur={event=>{this.checkclientid(event)}} value={this.getclientid()} onChange={event=>{this.handleclientid(event.target.value)}} /></div>
-         {this.showclienturl()}
-         <div className="register-full">Company <br/><input type="text" className="regular-field"   name="company"  onChange={event=>{this.handlecompany(event.target.value)}} value={this.getcompany()} /></div>
-         <div className="register-full">Address <br/><input type="text" className="regular-field"   name="address"   onChange={event=>{this.handleaddress(event.target.value)}}value={this.getaddress()}/></div>
-         <div className="register-half">City<br/><input type="text" className="regular-field"  name="city" value={this.getcity()}  onChange={event=>{this.handlecity(event.target.value)}} /></div>
-         <div className="register-half">State <br/><input type="text" className="regular-field"  name="contactstate" value={this.getcontactstate()}  onChange={event=>{this.handlecontactstate(event.target.value)}}/></div>
-         <div className="register-half">Zipcode<br/><input type="text" className="regular-field"  name="zipcode" value={this.getzipcode()}  onChange={event=>{this.handlezipcode(event.target.value)}}/></div>
-         <div className="register-half">Phone Number <br/><input type="text" className="regular-field" name="phone" value={this.getphonenumber()}  onChange={event=>{this.handlephonenumber(event.target.value)}} /></div>
-         <div className="register-full">Email Address<br/><input type="text" className="regular-field"  name="email" value={this.getemailaddress()}  onChange={event=>{this.handleemailaddress(event.target.value)}}/></div>
-         <div className="register-full">Password <br/><input type="password" className="regular-field" name="password" onChange={event=>{this.handlepassword(event.target.value)}} value={this.state.password}/></div>
-         <div className="emailsignup"><button id="btnsignupemail"> {emailSignup()}</button> </div>
-         <div className="register-full"><input type="hidden" name="gender"  value={this.state.gender}/></div>
-         
-        </div>
+                <div className="register-full">{this.state.message}  </div>
+                <div className="gender-container">
+                    <button type="button" className="newradio" id="btnmale" onClick={event => { this.handlemale(event) }}>{this.returnmale()} </button> Male
+            <button type="button" className="newradio" id="btnfemale" onClick={event => { this.handlefemale(event) }}> {this.returnfemale()}</button> Female
+            <input type="hidden" name="gender" value={this.getgender()} />
+                </div>
+                <div className="register-half">First Name<br /><input type="text" className="regular-field" name="firstname" onChange={event => { this.handlefirstname(event.target.value) }} value={this.getfirstname()} /></div>
+                <div className="register-half">Last Name <br /><input type="text" className="regular-field" name="lastname" onChange={event => { this.handlelastname(event.target.value) }} value={this.getlastname()} /></div>
+                <div className="register-full">Client ID <br /><input type="text" className="regular-field" name="clientid"
+                    onFocus={event => { this.checkclientid(event) }}
+                    onBlur={event => { this.checkclientid(event) }}
+                    value={this.getclientid()}
+                    onChange={event => { this.handleclientid(event.target.value) }} /></div>
+                {this.showclienturl()}
+                <div className="register-full">Company <br /><input type="text" className="regular-field" name="company" onChange={event => { this.handlecompany(event.target.value) }} value={this.getcompany()} /></div>
+                <div className="register-full">Address <br /><input type="text" className="regular-field" name="address" onChange={event => { this.handleaddress(event.target.value) }} value={this.getaddress()} /></div>
+                <div className="register-half">City<br /><input type="text" className="regular-field" name="city" value={this.getcity()} onChange={event => { this.handlecity(event.target.value) }} /></div>
+                <div className="register-half">State <br /><input type="text" className="regular-field" name="contactstate" value={this.getcontactstate()} onChange={event => { this.handlecontactstate(event.target.value) }} /></div>
+                <div className="register-half">Zipcode<br /><input type="text" className="regular-field" name="zipcode" value={this.getzipcode()} onChange={event => { this.handlezipcode(event.target.value) }} /></div>
+                <div className="register-half">Phone Number <br /><input type="text" className="regular-field" name="phone" value={this.getphonenumber()} onChange={event => { this.handlephonenumber(event.target.value) }} /></div>
+                <div className="register-full">Email Address<br /><input type="text" className="regular-field" name="email" value={this.getemailaddress()} onChange={event => { this.handleemailaddress(event.target.value) }} /></div>
+                <div className="register-full">Password <br /><input type="password" className="regular-field" name="password" onChange={event => { this.handlepassword(event.target.value) }} value={this.state.password} /></div>
+                <div className="emailsignup"><button id="btnsignupemail"> {emailSignup()}</button> </div>
+                <div className="register-full"><input type="hidden" name="gender" value={this.state.gender} /></div>
+
+            </div>
         </form>)
     }
 }
--- src/components/register.js
+++ src/components/register.js
@@ -106,48 +106,48 @@ class Register extends Component {
         }
     }
     render() {
-        const formaction = `${process.env.REACT_APP_SERVER_API}/api/clientregister`
+        const formaction = `${process.env.REACT_APP_SERVER_API}/clientregister`
         const googleredirect = `${process.env.REACT_APP_SERVER_API}/oauth20/google/login`;
         const googlescope = `https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=${googleredirect}&prompt=consent&response_type=code&client_id=${process.env.REACT_APP_GOOGLE_CLIENTID}&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fplus.login+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fplus.login+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fplus.me+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fplus.login+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fplus.me+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fplus.me+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email&access_type=offline`
         const yahooredirect = `${process.env.REACT_APP_SERVER_API}/oauth20/yahoo/login`;
         const yahooscope = `https://api.login.yahoo.com/oauth2/request_auth?redirect_uri=${yahooredirect}&prompt=consent&response_type=code&client_id=${process.env.REACT_APP_YAHOO_CLIENTID}`
-        return (<form action={formaction} method="post" onSubmit={event=>{this.handlesubmit(event)}}>
-        <div class="register-container">
-        <div class="register-google">
-             <a href={googlescope}><button id="btnregisterwithgoogle" type="button">{googleSignup()}</button></a>
+        return (<form action={formaction} method="post" onSubmit={event => { this.handlesubmit(event) }}>
+            <div class="register-container">
+                <div class="register-google">
+                    <a href={googlescope}><button id="btnregisterwithgoogle" type="button">{googleSignup()}</button></a>
+                </div>
+
+                <div className="register-google">
+
+                    <a href={yahooscope}>
+                        <button className="btnregister"> {yahooIcon()} </button>
+                    </a>
+
+                </div>
+                <div class="register-title">
+                    Register New Account By Email
         </div>
-        
-        <div className="register-google">
-            
-                <a href={yahooscope}>
-                   <button className="btnregister"> {yahooIcon()} </button>
-                </a>
-             
+                <div className="register-full">{this.state.message}  </div>
+                <div className="gender-container">
+                    <button type="button" className="newradio" id="btnmale" onClick={event => { this.handlemale(event) }}>{this.returnmale()} </button> Male
+            <button type="button" className="newradio" id="btnfemale" onClick={event => { this.handlefemale(event) }}> {this.returnfemale()}</button> Female
         </div>
-        <div class="register-title">
-        Register New Account By Email
-        </div>
-        <div className="register-full">{this.state.message}  </div>
-        <div className="gender-container">
-            <button type="button" className="newradio" id="btnmale" onClick={event=>{this.handlemale(event)}}>{this.returnmale()} </button> Male
-            <button type="button" className="newradio" id="btnfemale" onClick={event=>{this.handlefemale(event)}}> {this.returnfemale()}</button> Female
-        </div>
-        <div className="register-half">First Name<br/><input type="text" className="regular-field" name="firstname" onChange={event=>{this.handlefirstname(event.target.value)}} value={this.state.firstname} /></div>
-         <div className="register-half">Last Name <br/><input type="text" className="regular-field" name="lastname"  onChange={event=>{this.handlelastname(event.target.value)}} value={this.state.lastname}/></div>
-         <div className="register-full">Client ID <br/><input type="text" className="regular-field" name="clientid" onFocus={event=>{this.checkclientid(event)}} onBlur={event=>{this.checkclientid(event)}} value={this.state.clientid} onChange={event=>{this.handleclientid(event.target.value)}} /></div>
-         {this.showclienturl()}
-         <div className="register-full">Company <br/><input type="text" className="regular-field"   name="company"  onChange={event=>{this.handlecompany(event.target.value)}} value={this.state.company} /></div>
-         <div className="register-full">Address <br/><input type="text" className="regular-field"   name="address"   onChange={event=>{this.handleaddress(event.target.value)}}value={this.state.address}/></div>
-         <div className="register-half">City<br/><input type="text" className="regular-field"  name="city" value={this.state.city}  onChange={event=>{this.handlecity(event.target.value)}} /></div>
-         <div className="register-half">State <br/><input type="text" className="regular-field"  name="contactstate" value={this.state.contactstate}  onChange={event=>{this.handlecontactstate(event.target.value)}}/></div>
-         <div className="register-half">Zipcode<br/><input type="text" className="regular-field"  name="zipcode" value={this.state.zipcode}  onChange={event=>{this.handlezipcode(event.target.value)}}/></div>
-         <div className="register-half">Phone Number <br/><input type="text" className="regular-field" name="phonenumber" value={this.state.phonenumber}  onChange={event=>{this.handlephonenumber(event.target.value)}} /></div>
-         <div className="register-full">Email Address<br/><input type="text" className="regular-field"  name="emailaddress" value={this.state.emailaddress}  onChange={event=>{this.handleemailaddress(event.target.value)}}/></div>
-         <div className="register-full">Password <br/><input type="password" className="regular-field" name="password" onChange={event=>{this.handlepassword(event.target.value)}} value={this.state.password}/></div>
-         <div className="emailsignup"><button id="btnsignupemail"> {emailSignup()}</button> </div>
-         <div className="register-full"><input type="hidden" name="gender"  value={this.state.gender}/></div>
-         
-        </div>
+                <div className="register-half">First Name<br /><input type="text" className="regular-field" name="firstname" onChange={event => { this.handlefirstname(event.target.value) }} value={this.state.firstname} /></div>
+                <div className="register-half">Last Name <br /><input type="text" className="regular-field" name="lastname" onChange={event => { this.handlelastname(event.target.value) }} value={this.state.lastname} /></div>
+                <div className="register-full">Client ID <br /><input type="text" className="regular-field" name="clientid" onFocus={event => { this.checkclientid(event) }} onBlur={event => { this.checkclientid(event) }} value={this.state.clientid} onChange={event => { this.handleclientid(event.target.value) }} /></div>
+                {this.showclienturl()}
+                <div className="register-full">Company <br /><input type="text" className="regular-field" name="company" onChange={event => { this.handlecompany(event.target.value) }} value={this.state.company} /></div>
+                <div className="register-full">Address <br /><input type="text" className="regular-field" name="address" onChange={event => { this.handleaddress(event.target.value) }} value={this.state.address} /></div>
+                <div className="register-half">City<br /><input type="text" className="regular-field" name="city" value={this.state.city} onChange={event => { this.handlecity(event.target.value) }} /></div>
+                <div className="register-half">State <br /><input type="text" className="regular-field" name="contactstate" value={this.state.contactstate} onChange={event => { this.handlecontactstate(event.target.value) }} /></div>
+                <div className="register-half">Zipcode<br /><input type="text" className="regular-field" name="zipcode" value={this.state.zipcode} onChange={event => { this.handlezipcode(event.target.value) }} /></div>
+                <div className="register-half">Phone Number <br /><input type="text" className="regular-field" name="phonenumber" value={this.state.phonenumber} onChange={event => { this.handlephonenumber(event.target.value) }} /></div>
+                <div className="register-full">Email Address<br /><input type="text" className="regular-field" name="emailaddress" value={this.state.emailaddress} onChange={event => { this.handleemailaddress(event.target.value) }} /></div>
+                <div className="register-full">Password <br /><input type="password" className="regular-field" name="password" onChange={event => { this.handlepassword(event.target.value) }} value={this.state.password} /></div>
+                <div className="emailsignup"><button id="btnsignupemail"> {emailSignup()}</button> </div>
+                <div className="register-full"><input type="hidden" name="gender" value={this.state.gender} /></div>
+
+            </div>
         </form>)
     }
 }
